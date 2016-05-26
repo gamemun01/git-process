a4141c1b39bb7de7bbbabe4b2046724ba34ac70e
O.Chaloemchai <chaloemchai@arms-asia.com>
2016-05-26 16:21:40 +0700

    1st init

---
+++ Calc.php
@@ -0,0 +1,24 @@
+<?php
+
+class Calc {
+    private $hello;
+    private $goodbye;
+
+    function __construct($hello) {
+        $this->hello = $hello;
+    }
+
+    function Calc() {
+        for ($i = 0; $i <= $this->hello; $i++) {
+            $this->goodbye = $this->goodbye + $i;
+        }
+    }
+
+//    function getGoodBye() {
+//        return $this->goodbye . PHP_EOL;
+//    }
+
+    function getReject() {
+        return $this->goodbye . PHP_EOL;
+    }
+}
---
+++ Client.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: hirosige
+ * Date: 5/23/16
+ * Time: 2:49 PM
+ */
+
+require_once "Calc.php";
+
+$calc = new Calc(10);
+$calc->Calc();
+
+//echo $calc->getGoodBye();
+
+echo $calc->getReject();
